eda8a7f23d2301836b2aa89214fc8ddbf21d56c0
Kartik Bansal <kartikbansal945@gmail.com>
2017-03-19 23:16:40 +0530

    Changed the DB URL to use EC2

--- config/devConfig.js
+++ config/devConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-	dburi: 'mongodb://localhost/techpress',
+	dburi: 'mongodb://Kartik:titanicha1.@ec2-35-154-211-190.ap-south-1.compute.amazonaws.com:27017/techpress',
 	secret_key: 'its_MEAN_lol!'
 }
