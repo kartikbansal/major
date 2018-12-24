5650568559b138f298c18a65d09150c04c2a76b0
Kartik Bansal <kartikbansal945@gmail.com>
2018-12-24 14:41:34 +0530

    Update devConfig.js

--- config/devConfig.js
+++ config/devConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-	dburi: 'mongodb://Kartik:titanicha1.@ec2-35-154-211-190.ap-south-1.compute.amazonaws.com:27017/techpress',
+	dburi: 'mongodb://localhost/techpress',
 	secret_key: 'its_MEAN_lol!'
 }
